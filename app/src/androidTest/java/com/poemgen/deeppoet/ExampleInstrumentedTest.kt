67fe7e7c51427698cd60fd36cba14c92afdcabe8
jordankettles <70495950+jordankettles@users.noreply.github.com>
2021-08-13 17:04:35 +1200

    Update App Icon

--- app/src/androidTest/java/com/poemgen/deeppoet/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/poemgen/deeppoet/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.poemgen.mockspire", appContext.packageName)
+        assertEquals("com.poemgen.deeppoet", appContext.packageName)
     }
 }
