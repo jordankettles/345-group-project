378a17687de6e6f6a737320edd98a5bc5f25d6c6
mf284 <fukma284@student.otago.ac.nz>
2021-09-22 23:04:26 +1200

    Altered HelpActivity and LogActivity tests.

--- app/src/androidTest/java/com/poemgen/deeppoet/HelpInstrumentedTest.kt
+++ app/src/androidTest/java/com/poemgen/deeppoet/HelpInstrumentedTest.kt
@@ -3,9 +3,12 @@ package com.poemgen.deeppoet
 import androidx.test.core.app.launchActivity
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
+import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.intent.Intents.intended
 import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.After
@@ -25,7 +28,7 @@ class HelpInstrumentedTest {
      */
     @Before
     fun before() {
-        Intents.init()
+//        Intents.init()
     }
 
     /**
@@ -34,7 +37,7 @@ class HelpInstrumentedTest {
      */
     @After
     fun after() {
-        Intents.release()
+//        Intents.release()
     }
 
 
@@ -43,9 +46,20 @@ class HelpInstrumentedTest {
      */
     @Test
     fun testShowMainButton() {
-        launchActivity<HelpActivity>()
+        launchActivity<MainActivity>()
+        onView(withId(R.id.hamburgerButton)).perform(click())
+        onView(withId(R.id.helpButton)).perform(click())
+        android.os.SystemClock.sleep(2500)
+
+        // Hamburger button should not be visible while on help screen.
+        onView(withId(R.id.hamburgerButton)).check(doesNotExist())
+
+        android.os.SystemClock.sleep(1000)
         onView(withId(R.id.showMainHelp)).perform(click())
-        android.os.SystemClock.sleep(3000)
-        intended(hasComponent(MainActivity::class.java.getName()))
+
+        // Can't check if intent is launched, because no intent is launched.
+        // Check if view is visible instead, as a proxy.
+        onView(withId(R.id.hamburgerButton)).check(matches(isDisplayed()))
+
     }
 }
--- app/src/androidTest/java/com/poemgen/deeppoet/PoemLogInstrumentedTest.kt
+++ app/src/androidTest/java/com/poemgen/deeppoet/PoemLogInstrumentedTest.kt
@@ -2,10 +2,17 @@ package com.poemgen.deeppoet
 
 import androidx.test.core.app.launchActivity
 import androidx.test.espresso.Espresso
+import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
+import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.intent.matcher.IntentMatchers
 import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.After
 import org.junit.Before
@@ -24,7 +31,7 @@ class PoemLogInstrumentedTest {
      */
     @Before
     fun before() {
-        Intents.init()
+//        Intents.init()
     }
 
     /**
@@ -33,7 +40,7 @@ class PoemLogInstrumentedTest {
      */
     @After
     fun after() {
-        Intents.release()
+//        Intents.release()
     }
 
 
@@ -42,9 +49,20 @@ class PoemLogInstrumentedTest {
      */
     @Test
     fun clickShowMainButton() {
-        launchActivity<LogDisplayActivity>()
-        Espresso.onView(ViewMatchers.withId(R.id.showMain)).perform(ViewActions.click())
-        android.os.SystemClock.sleep(3000)
-        Intents.intended(IntentMatchers.hasComponent(MainActivity::class.java.getName()))
+        launchActivity<MainActivity>()
+        onView(withId(R.id.hamburgerButton)).perform(click())
+        onView(withId(R.id.showLogButton)).perform(click())
+        android.os.SystemClock.sleep(2500)
+
+        // Hamburger button should not be visible while on help screen.
+        onView(withId(R.id.hamburgerButton)).check(doesNotExist())
+
+        android.os.SystemClock.sleep(1000)
+        onView(withId(R.id.showMain)).perform(click())
+
+        // Can't check if intent is launched, because no intent is launched.
+        // Check if view is visible instead, as a proxy.
+        onView(withId(R.id.hamburgerButton)).check(matches(isDisplayed()))
+
     }
 }
