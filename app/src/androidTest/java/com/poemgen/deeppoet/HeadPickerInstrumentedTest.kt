f5e90434cd3cef3b584c3c9a68223f44e7a8d709
mf284 <fukma284@student.otago.ac.nz>
2021-09-24 21:15:01 +1200

    Starting second head picker test. Currently not working.

--- app/src/androidTest/java/com/poemgen/deeppoet/HeadPickerInstrumentedTest.kt
+++ app/src/androidTest/java/com/poemgen/deeppoet/HeadPickerInstrumentedTest.kt
@@ -1,11 +1,18 @@
 package com.poemgen.deeppoet
 
 import androidx.lifecycle.Lifecycle
+import androidx.recyclerview.widget.RecyclerView
 import androidx.test.core.app.launchActivity
+import androidx.test.espresso.Espresso.onData
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.poemgen.deeppoet.util.HeadCollection
+import org.hamcrest.EasyMock2Matchers.equalTo
+import org.hamcrest.Matcher
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -19,4 +26,13 @@ class HeadPickerInstrumentedTest {
         android.os.SystemClock.sleep(1000)
         assert(scenario.state == Lifecycle.State.DESTROYED)
     }
+
+    @Test
+    fun testHeadPickerInventory() {
+        launchActivity<HeadPickerActivity>()
+
+//        onView(withId(R.id.rvHeadPickerList)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
+//        assert(HeadCollection.currentHeadIndex == 0)
+    }
+
 }
