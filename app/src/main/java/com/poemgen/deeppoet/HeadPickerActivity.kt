875b8ad52aa6f687bb36615a3979afec65d38742
mf284 <fukma284@student.otago.ac.nz>
2021-09-24 20:17:07 +1200

    Allow returning to main from head picker.

--- app/src/main/java/com/poemgen/deeppoet/HeadPickerActivity.kt
+++ app/src/main/java/com/poemgen/deeppoet/HeadPickerActivity.kt
@@ -2,10 +2,16 @@ package com.poemgen.deeppoet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class HeadPickerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_head_picker)
+
+        val button = findViewById<Button>(R.id.showMain_HeadPicker)
+        button.setOnClickListener{
+            finish()
+        }
     }
 }
